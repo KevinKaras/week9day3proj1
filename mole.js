be8f6ce4965897df17e445766c4bc54ed0029b0a
KevinKaras <kevinkaras2@gmail.com>
2021-02-10 16:43:56 -0600

    added whacked attribute

--- mole.js
+++ mole.js
@@ -1,33 +1,40 @@
 // Write your JS here.
 window.addEventListener("DOMContentLoaded", event =>{
+
+
     function getRandomInt(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
       }
-      //we need to put this function in something or call it in something
+      
     let moles = document.getElementsByClassName("wgs__mole-head--hidden");
-    //let randomTarget = getRandomInt(0, 8);
-    //console.log(randomTarget)
-    console.log(moles);
     
-    function popUpRandomMole(moles){
+    // console.log(moles);
+    
+    function popUpRandomMole(){
         let randomTarget = getRandomInt(0, 8);
-        // let poppingMole = moles.item(randomTarget);
-        // console.log(poppingMole)
-        // let head = poppingMole.getElementsByClassName('wgs__mole-head--hidden')
-        // //call class.remove & class.add to remove
-        // //console.log(head)
-        // head = head.item(0);
-        // console.log(head)
+        let moles = document.querySelectorAll(".wgs__mole-head:not(.wgs__mole-head--whacked)");
+
+        // while(moles[randomTarget])
+        
+        
+        
         const head = moles[randomTarget];
+        
+        
         head.classList.remove('wgs__mole-head--hidden')
-        // head.classList.add('wgs__mole-head')
-        setTimeout(()=>hideMole(head),1000);
+        
+        head.addEventListener("click", event =>{
+
+            head.classList.add("wgs__mole-head--whacked")
+            hideMole(head)
+        })
+        setTimeout(()=>hideMole(head), 3000);
 
     }
     function hideMole(head){
-        // head.classList.remove('wgs__mole-head')
+        
         head.classList.add('wgs__mole-head--hidden')
         setTimeout(()=>popUpRandomMole(moles), 1000);
     }
