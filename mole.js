126af8b816c9523e13b8ed24ab1d3457976bafc6
KevinKaras <kevinkaras2@gmail.com>
2021-02-10 13:12:16 -0600

    lunch time

--- mole.js
+++ mole.js
@@ -1,1 +1,15 @@
 // Write your JS here.
+window.addEventListener("DOMContentLoaded", event =>{
+
+    let moles = document.querySelectorAll("wgs");
+    
+    console.log(moles);
+    function popUpRandomMole(){
+
+    }
+
+
+
+
+
+})
